c719adac218ecbf5198f83b22b88157dbd72c245
Patrick <p.girnghuber@icloud.com>
2023-06-29 21:44:02 +0000

    added name and email to git setup

--- build/init-script.sh
+++ build/init-script.sh
@@ -5,4 +5,12 @@ if [ -n "$GIT_USERNAME" ] && [ -n "$GIT_PASSWORD" ]; then
     echo "https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com" > ~/.git-credentials
 fi
 
+if [ -n "$GIT_NAME" ]; then
+    git config --global user.name $GIT_NAME
+fi
+
+if [ -n "$GIT_EMAIL" ]; then
+    git config --global user.email $GIT_EMAIL
+fi
+
 exec /bin/zsh
