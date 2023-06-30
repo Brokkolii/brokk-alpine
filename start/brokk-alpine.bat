d68738b9f9a11807f79d3d8640b76632ff3108a7
Patrick Girnghuber <p.girnghuber@icloud.com>
2023-06-30 10:55:34 +0000

    port throughpass

--- start/brokk-alpine.bat
+++ start/brokk-alpine.bat
@@ -1,9 +1,18 @@
 @ECHO OFF
 
 SET env_file=%~dp0..\.env
+SET ports_file=%~dp0..\.ports
 
+SET ports_args=
+IF EXIST %ports_file% (
+	FOR /F "tokens=*" %%a IN (%ports_file%) DO (
+		SET ports_args=%ports_args% -p %%a
+	)
+)
+ECHO %ports_args%
+pause
 IF EXIST %env_file% (
-    docker run -it --rm --env-file %env_file% -v %cd%:/workdir brokkolii/brokk-alpine
+    docker run -it --rm --env-file %env_file% %ports_args% -v %cd%:/workdir brokkolii/brokk-alpine
 ) ELSE (
-    docker run -it --rm -v %cd%:/workdir brokkolii/brokk-alpine
+    docker run -it --rm %ports_args% -v %cd%:/workdir brokkolii/brokk-alpine
 )
