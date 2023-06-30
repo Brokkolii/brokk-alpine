e24cef84f9d201c8366b620445281b9c70b39afa
Patrick Girnghuber <63921254+Brokkolii@users.noreply.github.com>
2023-06-30 07:33:17 +0200

    variable doesnt exits anymore

--- start/brokk-alpine.bat
+++ start/brokk-alpine.bat
@@ -5,5 +5,5 @@ SET env_file=%~dp0..\.env
 IF EXIST %env_file% (
     docker run -it --rm --env-file %env_file% -v %cd%:/workdir brokkolii/brokk-alpine
 ) ELSE (
-    docker run -it --rm -v %cd%:/workdir %image%
+    docker run -it --rm -v %cd%:/workdir brokkolii/brokk-alpine
 )
